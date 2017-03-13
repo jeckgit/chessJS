57793678879d54dd475f3b0d224686634a1f591e
joerg <joerg@joerg-pc.Speedport_W_724V_Typ_A_05011603_05_020>
2017-03-13 22:22:42 +0100

    board added

---
+++ js/board.js
@@ -0,0 +1,8 @@
+var GameBoard = {};
+GameBoard.pieces = new Array(BRD_SQ_NUM);
+GameBoard.side = COLOURS.WHITE;
+GameBoard.fiftyMove = 0;
+GameBoard.hisPly = 0;
+GameBoard.Ply = 0;
+GameBoard.castlePerm = 0;
+GameBoard.material = new Array(2);  // White , black material
